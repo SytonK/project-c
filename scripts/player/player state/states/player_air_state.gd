3ece60426b1296a41b879e513ca2601fcec14084
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-04-28 17:28:06 +0300

    feat/calculate transition from air state

--- scripts/player/player state/states/player_air_state.gd
+++ scripts/player/player state/states/player_air_state.gd
@@ -4,3 +4,9 @@ func process(delta: float) -> void:
 	player.gravity.apply_gravity(delta)
 	
 	player.move_and_slide()
+	
+	_calculate_transition()
+
+func _calculate_transition() -> void:
+	if player.is_on_floor():
+		transition.emit(PlayerState.GROUND)
