9c13ec12dec9c5511d48ea05d6b56da65bbb83c6
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-04-28 18:04:55 +0300

    ref/assert owner character body 2d

--- scripts/components/movement/jump_action.gd
+++ scripts/components/movement/jump_action.gd
@@ -3,5 +3,5 @@ class_name JumpAction extends Node
 @export var jump_force: float
 
 func jump() -> void:
-	assert(owner is CharacterBody2D)
+	assert(owner is CharacterBody2D, "Owner of the jump action component must be of type CharacterBody2D")
 	owner.velocity.y = -jump_force
