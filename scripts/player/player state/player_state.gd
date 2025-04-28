0c72601d03f3cd82f25a3302de6e97e02652c6dc
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-04-28 17:02:36 +0300

    feat/init player state

---
+++ scripts/player/player state/player_state.gd
@@ -0,0 +1,10 @@
+class_name PlayerState extends State
+
+const GROUND = "Ground"
+
+var player: Player
+
+func _ready() -> void:
+	await owner.ready
+	player = owner as Player
+	assert(player != null, "The PlayerState was used on an owner that is not a Player")
