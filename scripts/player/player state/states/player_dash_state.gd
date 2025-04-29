7a513d778fcf0df64bab528ecaee2c43ea9acfc9
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-04-29 19:19:14 +0300

    feat/stop dash with a jump

--- scripts/player/player state/states/player_dash_state.gd
+++ scripts/player/player state/states/player_dash_state.gd
@@ -8,15 +8,6 @@ func _ready() -> void:
 	super._ready()
 	_init_dash_timer()
 
-func _init_dash_timer() -> void:
-	dash_timer = Timer.new()
-	dash_timer.one_shot = true
-	dash_timer.timeout.connect(_on_dash_timer_timeout)
-	add_child(dash_timer)
-
-func _on_dash_timer_timeout() -> void:
-	player.calculate_transition()
-
 func enter(_previuse_state_name: String = "", data: Dictionary = {}) -> void:
 	assert(data.has("dash_duration"), "dash have no dash_duration")
 	dash_timer.wait_time = data["dash_duration"]
@@ -27,7 +18,31 @@ func physics_process(_delta: float) -> void:
 	
 	_calculate_early_exit()
 
+func input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		_try_jump()
+
+func _init_dash_timer() -> void:
+	dash_timer = Timer.new()
+	dash_timer.one_shot = true
+	dash_timer.timeout.connect(_on_dash_timer_timeout)
+	add_child(dash_timer)
+
+func _on_dash_timer_timeout() -> void:
+	player.calculate_transition()
+
 func _calculate_early_exit() -> void:
 	if player.velocity.length() < MIN_VELOCITY:
-		dash_timer.stop()
-		player.calculate_transition()
+		_early_exit()
+
+func _early_exit() -> void:
+	dash_timer.stop()
+	player.calculate_transition()
+
+func _try_jump() -> void:
+	if player.is_on_floor():
+		player.jump_action.jump()
+		_early_exit()
+	else:
+		if player.air_jump_action.air_jump():
+			_early_exit()
