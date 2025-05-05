a00b522b5415c49ef6b40806e872ed1f8ee177f4
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-05 08:43:17 +0300

    feat/edge forgivness

--- scripts/player/player state/states/player_air_state.gd
+++ scripts/player/player state/states/player_air_state.gd
@@ -1,5 +1,11 @@
 extends PlayerState
 
+@onready var edge_forgiveness_timer: Timer = $EdgeForgivenessTimer
+var is_edge_forgiveness: bool = false
+
+func enter(_previuse_state_name: String = "", _data: Dictionary = {}) -> void:
+	_start_edge_forgivness()
+
 func process(delta: float) -> void:
 	player.gravity.apply_gravity(delta)
 	
@@ -15,7 +21,21 @@ func input(event: InputEvent) -> void:
 	if event.is_action_released("jump"):
 		player.stop_jump_action.stop_jump()
 	if event.is_action_pressed("jump"):
-		player.air_jump_action.air_jump()
+		if is_edge_forgiveness:
+			player.jump_action.jump()
+			is_edge_forgiveness = false
+		else:
+			player.air_jump_action.air_jump()
 	if event.is_action_pressed("dash"):
 		player.player_dash_action.cast()
 		
+
+func _start_edge_forgivness() -> void:
+	if player.velocity.y < 0:
+		return
+	
+	is_edge_forgiveness = true
+	edge_forgiveness_timer.start()
+
+func _on_edge_forgiveness_timer_timeout() -> void:
+	is_edge_forgiveness = false
