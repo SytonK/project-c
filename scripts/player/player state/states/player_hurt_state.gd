4c7d01e4a8c1e7c96fd0f4cea4ded6bcfcc88b67
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-07 10:40:26 +0300

    feat/player hurt state

--- scripts/player/player state/states/player_hurt_state.gd
+++ scripts/player/player state/states/player_hurt_state.gd
@@ -1,1 +1,14 @@
 extends PlayerState
+
+@onready var hurt_timer: Timer = $HurtTimer
+
+func enter(_previuse_state_name: String = "", _data: Dictionary = {}) -> void:
+	hurt_timer.start()
+
+func physics_process(delta: float) -> void:
+	player.gravity.apply_gravity(delta)
+	
+	player.move_and_slide()
+
+func _on_hurt_timer_timeout() -> void:
+	player.calculate_transition()
