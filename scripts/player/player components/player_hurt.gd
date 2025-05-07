438d7782d953021f92f2e45bbbbd2d5d089f3215
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-07 10:37:11 +0300

    feat/init player hurt state

--- scripts/player/player components/player_hurt.gd
+++ scripts/player/player components/player_hurt.gd
@@ -1,5 +1,11 @@
 extends Node
 
+var player: Player
+
+func _ready() -> void:
+	await owner.ready
+	player = owner as Player
+	assert(player != null, "The PlayerHurt component was used on an owner that is not a Player")
 
 func _on_hurtbox_hurt(attack_resource: AttackResource, defense_resource: DefenseResource) -> void:
-	print("player hurt: ", attack_resource.damage)
+	player.state_machine.transition(PlayerStates.HURT)
