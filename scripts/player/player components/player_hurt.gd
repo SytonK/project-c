69dab9a6d7d68c03e33bcd1e1cec4892010fc1e6
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-07 12:17:58 +0300

    feat/palyer invulnerable

--- scripts/player/player components/player_hurt.gd
+++ scripts/player/player components/player_hurt.gd
@@ -3,6 +3,9 @@ extends Node
 const PUSHBACK_FOCRCE: float = 250
 const PUSHUP_FORCE: float = -500
 
+@onready var invulnerable_timer: Timer = $InvulnerableTimer
+var is_invulnerable: bool = false
+
 var player: Player
 
 func _ready() -> void:
@@ -12,10 +15,24 @@ func _ready() -> void:
 
 
 func _on_hurtbox_hurt(hitbox: Hitbox, hurtbox: Hurtbox) -> void:
+	if is_invulnerable:
+		return
+	
 	player.state_machine.transition(PlayerStates.HURT)
+	
 	_launch_player(hitbox, hurtbox)
+	
+	_start_invulnerable()
 
 func _launch_player(hitbox: Hitbox, hurtbox: Hurtbox) -> void:
 	player.velocity = Vector2(
 		((hurtbox.global_position.x - hitbox.global_position.x)/abs(hurtbox.global_position.x - hitbox.global_position.x)) * PUSHBACK_FOCRCE
 		,PUSHUP_FORCE)
+
+func _start_invulnerable() -> void:
+	is_invulnerable = true
+	invulnerable_timer.start()
+
+
+func _on_invulnerable_timer_timeout() -> void:
+	is_invulnerable = false
