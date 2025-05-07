2cee84d86c507a6d98d4f958dd779507247bcd4d
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-07 18:39:29 +0300

    feat/health component

--- scripts/components/health/health.gd
+++ scripts/components/health/health.gd
@@ -1,1 +1,17 @@
-extends Node
+class_name Health extends Node
+
+signal value_changed(new_health: float)
+signal died()
+
+@export var max_value: float
+@onready var value: float = max_value : set = _set_value
+
+func _set_value(new_value: float) -> void:
+	var old: float = value
+	value = clampf(new_value, 0, max_value)
+	
+	if old != value:
+		value_changed.emit(new_value)
+	
+	if value == 0:
+		died.emit()
