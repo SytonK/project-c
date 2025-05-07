e81d442de80d140ad2e302bb86bd14ef5e9fe98a
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-07 10:54:17 +0300

    feat/player launch on hurt

--- scripts/player/player components/player_hurt.gd
+++ scripts/player/player components/player_hurt.gd
@@ -1,5 +1,8 @@
 extends Node
 
+const PUSHBACK_FOCRCE: float = 250
+const PUSHUP_FORCE: float = -500
+
 var player: Player
 
 func _ready() -> void:
@@ -8,5 +11,11 @@ func _ready() -> void:
 	assert(player != null, "The PlayerHurt component was used on an owner that is not a Player")
 
 
-func _on_hurtbox_hurt(_hitbox: Hitbox, _hurtbox: Hurtbox) -> void:
+func _on_hurtbox_hurt(hitbox: Hitbox, hurtbox: Hurtbox) -> void:
 	player.state_machine.transition(PlayerStates.HURT)
+	_launch_player(hitbox, hurtbox)
+
+func _launch_player(hitbox: Hitbox, hurtbox: Hurtbox) -> void:
+	player.velocity = Vector2(
+		((hurtbox.global_position.x - hitbox.global_position.x)/abs(hurtbox.global_position.x - hitbox.global_position.x)) * PUSHBACK_FOCRCE
+		,PUSHUP_FORCE)
