9e717ded79e011e5bc2adb6b94dc7a300308b976
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-08 13:01:46 +0300

    bug/block ready

--- scripts/player/actions/player_block_action.gd
+++ scripts/player/actions/player_block_action.gd
@@ -3,6 +3,7 @@ class_name PlayerBlockAction extends Ability
 var player: Player
 
 func _ready() -> void:
+	super._ready()
 	await owner.ready
 	player = owner as Player
 	assert(player != null, "The PlayerDashAction was used on an owner that is not a Player")
