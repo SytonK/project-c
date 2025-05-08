4bb2fbdf199907af7b54d83099a6ed06c0cd4c1f
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-08 11:32:06 +0300

    feat/visual block

--- scripts/player/player state/states/player_block_state.gd
+++ scripts/player/player state/states/player_block_state.gd
@@ -2,9 +2,13 @@ extends PlayerState
 
 @onready var block_timer: Timer = $BlockTimer
 
+@onready var block_sprite: Polygon2D = $"../../BlockSprite"
+
 func enter(_previuse_state_name: String = "", _data: Dictionary = {}) -> void:
 	block_timer.start()
+	block_sprite.visible = true
 
 
 func _on_block_timer_timeout() -> void:
 	player.calculate_transition()
+	block_sprite.visible = false
