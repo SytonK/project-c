0536261763c166c6616753108c353deb079725ae
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-08 17:16:29 +0300

    ref/ability gui

--- scripts/GUI/HUD/ability/ability_gui.gd
+++ scripts/GUI/HUD/ability/ability_gui.gd
@@ -1,6 +1,7 @@
 class_name AbilityGUI extends TextureProgressBar
 
 const SQUARE_ICON = preload("res://assets/temp assets/square-icon.png")
+const BACKGROUND_A: float = 0.3
 
 var ability: Ability 
 
@@ -8,18 +9,20 @@ func _ready() -> void:
 	set_process(false)
 	fill_mode = FillMode.FILL_CLOCKWISE
 	texture_under = SQUARE_ICON
-	tint_under.a = .3
+	tint_under.a = BACKGROUND_A
 
 func set_ability(new_ability: Ability) -> void:
 	ability = new_ability
-	value = 100
+	_set_value()
 	ability.casted.connect(_start)
 
 func _start() -> void:
 	set_process(true)
 
-func _process(delta: float) -> void:
-	value = ((ability.cooldown - ability.cooldown_timer.time_left) / ability.cooldown) * 100
+func _process(_delta: float) -> void:
+	_set_value()
 	if value >= max_value:
 		set_process(false)
-	
+
+func _set_value() -> void:
+	value = ((ability.cooldown - ability.cooldown_timer.time_left) / ability.cooldown) * 100
