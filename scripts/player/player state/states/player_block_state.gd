70405c949724a6775fb2f6554e02b9e31f915fae
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-08 13:05:36 +0300

    feat/diffrent logic if hit or missed block

--- scripts/player/player state/states/player_block_state.gd
+++ scripts/player/player state/states/player_block_state.gd
@@ -4,11 +4,30 @@ extends PlayerState
 
 @onready var block_sprite: Polygon2D = $"../../BlockSprite"
 
+var blocked: bool = false
+
 func enter(_previuse_state_name: String = "", _data: Dictionary = {}) -> void:
 	block_timer.start()
 	block_sprite.visible = true
+	blocked = false
 
+
+func exit() -> void:
+	if blocked:
+		_exit_after_blocking()
+	else:
+		_exit_after_miss_blocking()
 
 func _on_block_timer_timeout() -> void:
 	player.calculate_transition()
 	block_sprite.visible = false
+
+
+func _on_hurtbox_hurt(hitbox: Hitbox, hurtbox: Hurtbox) -> void:
+	blocked = true
+
+func _exit_after_blocking() -> void:
+	print('blocked')
+
+func _exit_after_miss_blocking() -> void:
+	print('did not block')
