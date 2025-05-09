ef39b51f6118cf429657c4bcd256b988e6981b64
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-09 19:03:15 +0300

    feat/weapon call implementation by name

--- scripts/player/weapon/weapon.gd
+++ scripts/player/weapon/weapon.gd
@@ -10,4 +10,79 @@ func attack(attack_type: AttackType) -> void:
 	var state_str: String = "ground" if player.state_machine.state.name == PlayerStates.GROUND else "air"
 	var attack_str: String = type_str + "_" + direction_str + "_" + state_str + "_attack"
 	
-	print(attack_str)
+	print(attack_str) #TODO remove
+	
+	assert(has_method(attack_str))
+	call(attack_str)
+
+func light_side_ground_attack() -> void:
+	pass
+
+func light_side_air_attack() -> void:
+	pass
+
+func light_up_ground_attack() -> void:
+	pass
+
+func light_up_air_attack() -> void:
+	pass
+
+func light_down_ground_attack() -> void:
+	pass
+
+func light_down_air_attack() -> void:
+	pass
+
+func heavy_side_ground_attack() -> void:
+	pass
+
+func heavy_side_air_attack() -> void:
+	pass
+
+func heavy_up_ground_attack() -> void:
+	pass
+
+func heavy_up_air_attack() -> void:
+	pass
+
+func heavy_down_ground_attack() -> void:
+	pass
+
+func heavy_down_air_attack() -> void:
+	pass
+
+func special_side_ground_attack() -> void:
+	pass
+
+func special_side_air_attack() -> void:
+	pass
+
+func special_up_ground_attack() -> void:
+	pass
+
+func special_up_air_attack() -> void:
+	pass
+
+func special_down_ground_attack() -> void:
+	pass
+
+func special_down_air_attack() -> void:
+	pass
+
+func dash_side_ground_attack() -> void:
+	pass
+
+func dash_side_air_attack() -> void:
+	pass
+
+func dash_up_ground_attack() -> void:
+	pass
+
+func dash_up_air_attack() -> void:
+	pass
+
+func dash_down_ground_attack() -> void:
+	pass
+
+func dash_down_air_attack() -> void:
+	pass
