bc4c3805e84238755c40f6ebb0faf361443cc464
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-12 08:54:36 +0300

    ref/type

--- scripts/player/weapon/weapon energy/weapon_energy.gd
+++ scripts/player/weapon/weapon energy/weapon_energy.gd
@@ -3,7 +3,7 @@ class_name WeaponEnergy
 signal used(amount: float)
 
 var max_value: float = 100
-var value: = 0 : set = _set_value
+var value: float = 0 : set = _set_value
 
 func _set_value(new_value: float) -> void:
 	var res = clamp(new_value, 0, max_value)
