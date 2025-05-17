2d625cfab2a8737045c0311a614e0246a951f070
SytonK <Syton98K@gmail.com>
2025-05-17 14:19:11 +0300

    feat/dual guns side attack

--- scripts/player/weapon/dual guns/light attacks/dual_guns_lisht_side.gd
+++ scripts/player/weapon/dual guns/light attacks/dual_guns_lisht_side.gd
@@ -1,9 +1,23 @@
 class_name DualGunsLishtSide extends Ability
 
+const DUAL_GUN_LIGHT_SIDE_BASE = preload("res://data/player/weapons/dual guns/dual_gun_light_side_base.tres")
 const DUAL_GUN_LIGHT_SIDE_0 = preload("res://data/player/weapons/dual guns/dual_gun_light_side_0.tres")
-const BASE_BULLET_SPEED: float = 1000
+const DUAL_GUN_LIGHT_SIDE_1 = preload("res://data/player/weapons/dual guns/dual_gun_light_side_1.tres")
+
+const BASE_BULLET_SPEED: float = 860
+const FAST_BULLET_SPEED: float = 2070
+
 const BULLET_LIFE_TIME: float = .4
+
 const BULLET_0 = preload("res://assets/sprites/player/weapons/dual guns/bullet 0.png")
+const BULLET_1 = preload("res://assets/sprites/player/weapons/dual guns/bullet 1.png")
+
+const COOLDOWN_0: float = .1
+const COOLDOWN_1: float = .7
+
+const ENERGY_COST: float = 15
+
+@onready var sequence: Sequence = $Sequence
 
 var player: Player
 var weapon: Weapon
@@ -16,6 +30,34 @@ func _ready() -> void:
 	player = weapon.player
 
 func _ability_effect() -> void:
-	var new_bullet := Bullet.new(DUAL_GUN_LIGHT_SIDE_0, BASE_BULLET_SPEED, Vector2(player.get_facing_direction_to_float(), 0), BULLET_LIFE_TIME, BULLET_0)
+	match sequence.value:
+		0: 
+			_attack_0()
+		1:
+			_attack_1()
+	
+	sequence.value += 1
+
+func _attack_0() -> void:
+	_spawn_bullet(DUAL_GUN_LIGHT_SIDE_0, BULLET_0)
+	
+	cooldown = COOLDOWN_0
+	
+func _attack_1() -> void:
+	_spawn_bullet(DUAL_GUN_LIGHT_SIDE_1, BULLET_1)
+	
+	cooldown = COOLDOWN_1
+
+func _spawn_bullet(p_attack_reasource: AttackResource, texture: Texture2D) -> void:
+	var attack_resourc: AttackResource
+	var speed: float
+	if weapon.weapon_energy.value >= ENERGY_COST:
+		attack_resourc = p_attack_reasource
+		speed = FAST_BULLET_SPEED
+		weapon.weapon_energy.value -= ENERGY_COST
+	else:
+		attack_resourc = DUAL_GUN_LIGHT_SIDE_BASE
+		speed = BASE_BULLET_SPEED
+	var new_bullet := Bullet.new(attack_resourc, speed, Vector2(player.get_facing_direction_to_float(), 0), BULLET_LIFE_TIME, texture)
 	new_bullet.global_position = weapon.global_position
 	get_tree().root.add_child(new_bullet)
