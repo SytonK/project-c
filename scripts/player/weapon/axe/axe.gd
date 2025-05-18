53652bec5402e700fd3d99c7bb9e4afeda75a41f
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-18 19:36:47 +0300

    feat/axe light up

--- scripts/player/weapon/axe/axe.gd
+++ scripts/player/weapon/axe/axe.gd
@@ -1,9 +1,16 @@
 extends Weapon
 
 @onready var light_side: AxeLightSide = $LightSide
+@onready var axe_light_up: AxeLightUp = $AxeLightUp
 
 func light_side_ground_attack() -> void:
 	light_side.cast()
 
 func light_side_air_attack() -> void:
 	light_side.cast()
+
+func light_up_ground_attack() -> void:
+	axe_light_up.cast()
+
+func light_up_air_attack() -> void:
+	axe_light_up.cast()
---
+++ scripts/player/weapon/axe/light attacks/axe_ligth_up.gd
@@ -0,0 +1,34 @@
+class_name AxeLightUp extends Ability
+
+const ENERGY_COST: float = 10
+const AXE_BASE_ATTACK = preload("res://data/player/weapons/axe/light/axe_base_attack.tres")
+const AXE_LIGHT_UP_HEAD = preload("res://data/player/weapons/axe/light/up/axe_light_up_head.tres")
+
+@onready var axe_head_shape: MultiRegionHitShape = $"../MultiRegionHitbox/AxeHeadShape"
+
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+
+var player: Player
+var weapon: Weapon
+
+func _ready() -> void:
+	super._ready()
+	assert(owner is Weapon, "weapon ability was used not on a weapon")
+	weapon = owner
+	await owner.ready
+	player = owner.player
+
+func _ability_effect() -> void:
+	_attack_setup()
+	animation_player.play("light_up")
+	if weapon.weapon_energy.value >= ENERGY_COST:
+		weapon.weapon_energy.value -= ENERGY_COST
+		axe_head_shape.attack_resource = AXE_LIGHT_UP_HEAD
+	else:
+		axe_head_shape.attack_resource = AXE_BASE_ATTACK
+
+
+func _attack_setup() -> void:
+	player.state_machine.transition(PlayerStates.ATTACK)
+	player.velocity.x = 0
+	player.velocity.y = max(player.velocity.y, 0)
