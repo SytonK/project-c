cce09766c8a23048786abe4a356be6a6ff0e130b
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-20 20:17:07 +0300

    feat/axe light down air

--- scripts/player/weapon/axe/axe.gd
+++ scripts/player/weapon/axe/axe.gd
@@ -3,6 +3,7 @@ extends Weapon
 @onready var light_side: AxeLightSide = $LightSide
 @onready var axe_light_up: AxeLightUp = $AxeLightUp
 @onready var axe_light_down_ground: AxeLightDownGround = $AxeLightDownGround
+@onready var axe_light_down_air: AxeLightDownAir = $AxeLightDownAir
 
 func light_side_ground_attack() -> void:
 	light_side.cast()
@@ -18,3 +19,6 @@ func light_up_air_attack() -> void:
 
 func light_down_ground_attack() -> void:
 	axe_light_down_ground.cast()
+
+func light_down_air_attack() -> void:
+	axe_light_down_air.cast()
