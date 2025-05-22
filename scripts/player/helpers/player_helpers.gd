66ab2ce7793b12c3b4bb2f9b9558a20a4146ca49
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-22 18:56:32 +0300

    ref/player helper type

--- scripts/player/helpers/player_helpers.gd
+++ scripts/player/helpers/player_helpers.gd
@@ -1,4 +1,4 @@
-class_name PlayerHelpers extends Node
+class_name PlayerHelpers
 
 static func get_player_owner(node: Node) -> Player:
 	assert(node.owner is Player, "The get_player_owner was used on an owner that is not a Player")
