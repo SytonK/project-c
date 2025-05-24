b81f9ab12e8b294ca4e201eb42cc2a5e96d32cb8
SytonK <Syton98K@gmail.com>
2025-05-24 12:37:14 +0300

    feat/dual_guns_light_down_ground

--- scripts/player/weapon/dual guns/light attacks/dual_guns_light_down_ground.gd
+++ scripts/player/weapon/dual guns/light attacks/dual_guns_light_down_ground.gd
@@ -2,8 +2,9 @@ class_name DualGunsLightDownGround extends Ability
 
 const DUAL_GUNS_LIGHT_DOWN_GROUND = preload("res://data/player/weapons/dual guns/light/down/dual_guns_light_down_ground.tres")
 const BULLET_0 = preload("res://assets/sprites/player/weapons/dual guns/bullet 0.png")
+const BULLET_1 = preload("res://assets/sprites/player/weapons/dual guns/bullet 1.png")
 const Y_DIRECTION: float = .25
-const ENERGY_COST: float = 15
+const ENERGY_COST: float = 10
 
 @onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
@@ -13,13 +14,14 @@ const ENERGY_COST: float = 15
 func _ability_effect() -> void:
 	_set_up_attack()
 	animation_player.play("light_down_ground")
-	_spawn_bullet()
+	_spawn_bullet(player.get_facing_direction_to_float(), BULLET_0)
+	_spawn_bullet(-player.get_facing_direction_to_float(), BULLET_1)
 
 func _set_up_attack() -> void:
 	player.state_machine.transition(PlayerStates.ATTACK)
 	player.velocity.x = 0
 
-func _spawn_bullet() -> void:
+func _spawn_bullet(x_direction: float, texture: Texture2D) -> void:
 	var attack_resourc: AttackResource
 	var speed: float
 	var bounce: bool = false
@@ -31,6 +33,6 @@ func _spawn_bullet() -> void:
 	else:
 		attack_resourc = Bullet.DUAL_GUNS_LIGHT_BASE
 		speed = Bullet.BASE_BULLET_SPEED
-	var new_bullet := Bullet.new(BULLET_0, Vector2(player.get_facing_direction_to_float(), Y_DIRECTION), attack_resourc, speed, bounce)
+	var new_bullet := Bullet.new(texture, Vector2(x_direction, Y_DIRECTION), attack_resourc, speed, bounce)
 	new_bullet.global_position = weapon.global_position
 	get_tree().root.add_child(new_bullet)
