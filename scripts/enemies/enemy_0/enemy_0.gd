548d88d5a695cc8ae4f0b65d1109fac54b53d5be
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-29 17:53:47 +0300

    feat/enemy_0 hurtbox

--- scripts/enemies/enemy_0/enemy_0.gd
+++ scripts/enemies/enemy_0/enemy_0.gd
@@ -4,3 +4,7 @@ extends CharacterBody2D
 
 func _on_health_died() -> void:
 	queue_free()
+
+
+func _on_hurtbox_hurt(attack_resource: AttackResource, _attack_position: Vector2, _defense_resource: DefenseResource, _defense_position: Vector2) -> void:
+	health.value -= attack_resource.damage
