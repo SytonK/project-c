3399d55faceedd69391efeb1ca114124ad166a65
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-29 17:20:29 +0300

    ref/weapon energy ui

--- scripts/GUI/HUD/weapon energy bar/weapon_energy_bar.gd
+++ scripts/GUI/HUD/weapon energy bar/weapon_energy_bar.gd
@@ -5,6 +5,7 @@ var weapon: Weapon : set = _set_weapon
 func _set_weapon(new_weapon: Weapon) -> void:
 	weapon = new_weapon
 	weapon.weapon_energy.changed.connect(_set_value)
+	_set_value(weapon.weapon_energy.value)
 
 func _set_value(new_value: float) -> void:
 	value = new_value
