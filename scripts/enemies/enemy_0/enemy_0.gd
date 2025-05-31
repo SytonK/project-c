4b2de06c9f537dfb217e5719532e60a3c90ea07e
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-05-31 11:55:33 +0300

    feat/add enemy_0 facing while moving

--- scripts/enemies/enemy_0/enemy_0.gd
+++ scripts/enemies/enemy_0/enemy_0.gd
@@ -4,14 +4,21 @@ extends CharacterBody2D
 
 @onready var health: Health = $Components/Health
 @onready var gravity: Gravity = $Components/Gravity
+@onready var facing_direction: FacingDirection = $Components/FacingDirection
 
 func _process(delta: float) -> void:
 	gravity.apply_gravity(delta)
 	move_and_slide()
-
+	look_at_movement_direction()
 
 func _on_health_died() -> void:
 	queue_free()
 
 func _on_hurtbox_hurt(attack_resource: AttackResource, _attack_position: Vector2, _defense_resource: DefenseResource, _defense_position: Vector2) -> void:
 	health.value -= attack_resource.damage
+
+func look_at_movement_direction() -> void:
+	if velocity.x > 0:
+		facing_direction.direction = FacingDirection.DIRECTIONS.RIGHT
+	elif velocity.x < 0:
+		facing_direction.direction = FacingDirection.DIRECTIONS.LEFT
