a02290fb965fdf5922dab83af30b12d49ba88045
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-02 21:36:17 +0300

    feat/init enemy_0 bt

--- scripts/components/behavior tree/trees/enemy 0/enemy_0_behavior_tree.gd
+++ scripts/components/behavior tree/trees/enemy 0/enemy_0_behavior_tree.gd
@@ -1,1 +1,9 @@
-class_name Enemy0BT extends BehaviorTree
+extends BehaviorTree
+
+@export var animation_player: AnimationPlayer
+
+@onready var play_animation_leaf: PlayAnimationLeaf = $SequenceComposite/PlayAnimationLeaf
+
+func _ready() -> void:
+	super._ready()
+	play_animation_leaf.animation_player = animation_player
