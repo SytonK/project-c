871c21493b961236a0b047fe2d224744bbdc3aef
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-03 16:57:39 +0300

    feat/enemy_0 attack

--- scripts/components/behavior tree/trees/enemy 0/enemy_0_behavior_tree.gd
+++ scripts/components/behavior tree/trees/enemy 0/enemy_0_behavior_tree.gd
@@ -5,17 +5,20 @@ extends BehaviorTree
 @export var facing_direction: FacingDirection
 @export var target: Node2D
 
-@onready var play_animation_leaf: PlayAnimationLeaf = $SequenceComposite/PlayAnimationLeaf
+@onready var play_animation_run: PlayAnimationLeaf = $SequenceComposite/PlayAnimationRun
 @onready var side_move_to_target_leaf: SideMoveToTargetLeaf = $SequenceComposite/SideMoveToTargetLeaf
 @onready var look_at_target_leaf: LookAtTargetLeaf = $SequenceComposite/LookAtTargetLeaf
+@onready var play_animation_attack: PlayAnimationLeaf = $SequenceComposite/PlayAnimationAttack
 
 func _ready() -> void:
 	super._ready()
 	await owner.ready
-	play_animation_leaf.animation_player = animation_player
+	play_animation_run.animation_player = animation_player
 	
 	side_move_to_target_leaf.side_movement_action = side_movement_action
 	side_move_to_target_leaf.target = target
 	
 	look_at_target_leaf.facing_direction = facing_direction
 	look_at_target_leaf.target = target
+	
+	play_animation_attack.animation_player = animation_player
