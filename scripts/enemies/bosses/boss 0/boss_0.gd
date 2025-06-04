5866c10f6f82a707fdd213bef94930bba619731b
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-05 01:10:51 +0300

    feat/boss 0 health

--- scripts/enemies/bosses/boss 0/boss_0.gd
+++ scripts/enemies/bosses/boss 0/boss_0.gd
@@ -4,5 +4,13 @@ extends Node2D
 
 @onready var boss_0_behavior_tree: Node = $Boss0BehaviorTree
 
+@onready var health: Health = $Compontnts/Health
+
 func _ready() -> void:
 	boss_0_behavior_tree.target = target
+
+func _on_hurtbox_hurt(attack_resource: AttackResource, _attack_position: Vector2, _defense_resource: DefenseResource, _defense_position: Vector2) -> void:
+	health.value -= attack_resource.damage
+
+func _on_health_died() -> void:
+	queue_free()
