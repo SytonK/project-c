632a2f440139b90b1f4e62704bd0bc124d9838e0
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-05 10:53:50 +0300

    feat/boss 0 side movement melle

--- scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
+++ scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
@@ -2,8 +2,11 @@ extends BehaviorTree
 
 @export var target: Player
 
+@export var side_movement_action: SideMovementAction
+
 @onready var in_range_melle: InRange = $SelectComposite/InRangeMelle
 @onready var in_range_mid: InRange = $SelectComposite/InRangeMid
+@onready var side_move_to_target_leaf: SideMoveToTargetLeaf = $SelectComposite/InRangeMelle/SequenceComposite/SideMoveToTargetLeaf
 
 func _ready() -> void:
 	super._ready()
@@ -15,3 +18,5 @@ func _ready() -> void:
 	in_range_mid.target = target
 	in_range_mid.source = owner
 	
+	side_move_to_target_leaf.target = target
+	side_move_to_target_leaf.side_movement_action = side_movement_action
