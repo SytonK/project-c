16ff3f48a5bdee3f4fbc302f03e3ae570d74b476
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-05 11:59:20 +0300

    feat/boss_0 look at taregt

--- scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
+++ scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
@@ -3,10 +3,12 @@ extends BehaviorTree
 @export var target: Player
 
 @export var side_movement_action: SideMovementAction
+@export var facing_direction: FacingDirection
 
 @onready var in_range_melle: InRange = $SelectComposite/InRangeMelle
 @onready var in_range_mid: InRange = $SelectComposite/InRangeMid
 @onready var side_move_to_target_leaf: SideMoveToTargetLeaf = $SelectComposite/InRangeMelle/SequenceComposite/SideMoveToTargetLeaf
+@onready var look_at_target_leaf: LookAtTargetLeaf = $SelectComposite/InRangeMelle/SequenceComposite/LookAtTargetLeaf
 
 func _ready() -> void:
 	super._ready()
@@ -20,3 +22,6 @@ func _ready() -> void:
 	
 	side_move_to_target_leaf.target = target
 	side_move_to_target_leaf.side_movement_action = side_movement_action
+	
+	look_at_target_leaf.target = target
+	look_at_target_leaf.facing_direction = facing_direction
