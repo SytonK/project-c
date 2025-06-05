a3870803705809fddc520e481ee2f91ff77f52ef
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-05 10:27:41 +0300

    feat/boss 0 gravity

--- scripts/enemies/bosses/boss 0/boss_0.gd
+++ scripts/enemies/bosses/boss 0/boss_0.gd
@@ -1,13 +1,19 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var target: Node2D
 
 @onready var boss_0_behavior_tree: Node = $Boss0BehaviorTree
 
 @onready var health: Health = $Compontnts/Health
+@onready var gravity: Gravity = $Compontnts/Gravity
 
 func _ready() -> void:
 	boss_0_behavior_tree.target = target
+
+func _physics_process(delta: float) -> void:
+	gravity.apply_gravity(delta)
+	
+	move_and_slide()
 
 func _on_hurtbox_hurt(attack_resource: AttackResource, _attack_position: Vector2, _defense_resource: DefenseResource, _defense_position: Vector2) -> void:
 	health.value -= attack_resource.damage
