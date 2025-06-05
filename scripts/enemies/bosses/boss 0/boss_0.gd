731a6a2e6f7071bb02651530f6a52e300bcd93bb
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-05 10:55:28 +0300

    feat/boss_0 look at movement direction

--- scripts/enemies/bosses/boss 0/boss_0.gd
+++ scripts/enemies/bosses/boss 0/boss_0.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 
 @onready var health: Health = $Compontnts/Health
 @onready var gravity: Gravity = $Compontnts/Gravity
+@onready var facing_direction: FacingDirection = $Compontnts/FacingDirection
 
 func _ready() -> void:
 	boss_0_behavior_tree.target = target
@@ -14,6 +15,14 @@ func _physics_process(delta: float) -> void:
 	gravity.apply_gravity(delta)
 	
 	move_and_slide()
+	
+	_look_at_movement_direction()
+
+func _look_at_movement_direction() -> void:
+	if velocity.x > 0:
+		facing_direction.direction = FacingDirection.DIRECTIONS.RIGHT
+	elif velocity.x < 0:
+		facing_direction.direction = FacingDirection.DIRECTIONS.LEFT
 
 func _on_hurtbox_hurt(attack_resource: AttackResource, _attack_position: Vector2, _defense_resource: DefenseResource, _defense_position: Vector2) -> void:
 	health.value -= attack_resource.damage
