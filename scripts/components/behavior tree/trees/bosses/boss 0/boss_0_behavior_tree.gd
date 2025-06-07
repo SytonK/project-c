8323d26eeb5fc2cc5ad6004c511a76f60881e163
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-07 21:02:00 +0300

    feat/bass_0 melee_attack_0

--- scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
+++ scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
@@ -4,11 +4,14 @@ extends BehaviorTree
 
 @export var side_movement_action: SideMovementAction
 @export var facing_direction: FacingDirection
+@export var animation_player: AnimationPlayer
 
-@onready var in_range_melle: InRange = $SelectComposite/InRangeMelle
+@onready var in_range_melle: InRange = $SelectComposite/MeleeSequenceComposite/InRangeMelle
+@onready var side_move_to_target_leaf: SideMoveToTargetLeaf = $SelectComposite/MeleeSequenceComposite/InRangeMelle/SequenceComposite/SideMoveToTargetLeaf
+@onready var look_at_target_leaf: LookAtTargetLeaf = $SelectComposite/MeleeSequenceComposite/InRangeMelle/SequenceComposite/LookAtTargetLeaf
+@onready var play_animation_leaf: PlayAnimationLeaf = $SelectComposite/MeleeSequenceComposite/PlayAnimationLeaf
+
 @onready var in_range_mid: InRange = $SelectComposite/InRangeMid
-@onready var side_move_to_target_leaf: SideMoveToTargetLeaf = $SelectComposite/InRangeMelle/SequenceComposite/SideMoveToTargetLeaf
-@onready var look_at_target_leaf: LookAtTargetLeaf = $SelectComposite/InRangeMelle/SequenceComposite/LookAtTargetLeaf
 
 func _ready() -> void:
 	super._ready()
@@ -25,3 +28,5 @@ func _ready() -> void:
 	
 	look_at_target_leaf.target = target
 	look_at_target_leaf.facing_direction = facing_direction
+	
+	play_animation_leaf.animation_player = animation_player
