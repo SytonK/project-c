f6db1c7c977fb377473b462aa5aa9ddda836d520
Sytonk <122539345+SytonK@users.noreply.github.com>
2025-06-07 21:05:09 +0300

    ref/boss_0_behavior renaming nodes

--- scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
+++ scripts/components/behavior tree/trees/bosses/boss 0/boss_0_behavior_tree.gd
@@ -7,9 +7,9 @@ extends BehaviorTree
 @export var animation_player: AnimationPlayer
 
 @onready var in_range_melle: InRange = $SelectComposite/MeleeSequenceComposite/InRangeMelle
-@onready var side_move_to_target_leaf: SideMoveToTargetLeaf = $SelectComposite/MeleeSequenceComposite/InRangeMelle/SequenceComposite/SideMoveToTargetLeaf
-@onready var look_at_target_leaf: LookAtTargetLeaf = $SelectComposite/MeleeSequenceComposite/InRangeMelle/SequenceComposite/LookAtTargetLeaf
-@onready var play_animation_leaf: PlayAnimationLeaf = $SelectComposite/MeleeSequenceComposite/PlayAnimationLeaf
+@onready var melee_side_move_to_target_leaf: SideMoveToTargetLeaf = $SelectComposite/MeleeSequenceComposite/InRangeMelle/SequenceComposite/MeleeSideMoveToTargetLeaf
+@onready var melee_look_at_target_leaf: LookAtTargetLeaf = $SelectComposite/MeleeSequenceComposite/InRangeMelle/SequenceComposite/MeleeLookAtTargetLeaf
+@onready var play_animation_leaf_melee_attack_0: PlayAnimationLeaf = $SelectComposite/MeleeSequenceComposite/PlayAnimationLeafMeleeAttack0
 
 @onready var in_range_mid: InRange = $SelectComposite/InRangeMid
 
@@ -23,10 +23,10 @@ func _ready() -> void:
 	in_range_mid.target = target
 	in_range_mid.source = owner
 	
-	side_move_to_target_leaf.target = target
-	side_move_to_target_leaf.side_movement_action = side_movement_action
+	melee_side_move_to_target_leaf.target = target
+	melee_side_move_to_target_leaf.side_movement_action = side_movement_action
 	
-	look_at_target_leaf.target = target
-	look_at_target_leaf.facing_direction = facing_direction
+	melee_look_at_target_leaf.target = target
+	melee_look_at_target_leaf.facing_direction = facing_direction
 	
-	play_animation_leaf.animation_player = animation_player
+	play_animation_leaf_melee_attack_0.animation_player = animation_player
